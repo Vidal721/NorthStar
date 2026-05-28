55ca682a251295e1a81a4ced31b05e0092f4a43a
Vidal721 <aprich1109@gmail.com>
2026-05-27 22:17:38 -0500

    Added different pages

---
+++ 935Sample/Example software/src/pages/pit.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+
+
+function app () {
+    return(
+        <h1>Hello Pit!</h1>
+    )
+}
+ export default app
---
+++ 935Sample/Example software/src/pages/vis.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+
+
+function app () {
+    return(
+        <h1>Hello Vis!</h1>
+    )
+}
+ export default app
