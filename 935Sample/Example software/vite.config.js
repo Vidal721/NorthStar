b17565b24fe70aed1426fa4aaf7b014e723516c6
Vidal721 <aprich1109@gmail.com>
2026-06-02 19:04:46 -0500

    Added splash screen, otherwise PWA works

--- 935Sample/Example software/vite.config.js
+++ 935Sample/Example software/vite.config.js
@@ -1,40 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "mask-icon.svg",
+        "apple-splash-640x1136.png",
+        "apple-splash-1242x2688.png",
+        "apple-splash-2048x2732.png",
+      ],
+
       manifest: {
-        name: 'Team 935 Scouting App',
-        short_name: '935 Scouting',
-        description: 'My awesome React Progressive Web App',
-        theme_color: '#000000',
-        background_color: '#ffffff',
-        display: 'standalone',
+        name: "Team 935 Scouting App",
+        short_name: "935 Scouting",
+        description: "My awesome React Progressive Web App",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
