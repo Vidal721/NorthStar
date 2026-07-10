a0efcf1ac865e8fdc32997bc8ae1713a3982d849
Vidal721 <aprich1109@gmail.com>
2026-07-09 18:27:20 -0600

    Update urlConfig.js

--- 935Sample/example-software/src/urlConfig.js
+++ 935Sample/example-software/src/urlConfig.js
@@ -5,9 +5,7 @@ export function useURL () {
         console.log("From url config Using https://taco-childhood-jailbreak.ngrok-free.dev as the backend url.");
         return "https://taco-childhood-jailbreak.ngrok-free.dev";
     } else {
-        //console.log("Using http://localhost:3000 as the backend url.");
-        //return "http://localhost:3000"
-        console.log("From url config Using https://taco-childhood-jailbreak.ngrok-free.dev as the backend url.");
-        return "https://taco-childhood-jailbreak.ngrok-free.dev";
+        console.log("Using http://localhost:3000 as the backend url.");
+        return "http://localhost:3000";
     }
 }
